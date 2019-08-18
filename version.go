09389e7628bd7037c43d046eb7b7386403c88004
Vault 13 <v73o@yandex.ru>
2019-08-18 20:33:24 +0300

    Update.

--- version.go
+++ version.go
@@ -19,21 +19,4 @@
 
 package reader
 
-import (
-	"bufio"
-	"io"
-)
-
-func New(
-	reader io.Reader,
-) *Reader {
-
-	var result *Reader
-
-	result = new(Reader)
-
-	result.reader = reader
-	result.bufioReader = bufio.NewReader(result.reader)
-
-	return result
-}
+const PackageVersion = "1.0"
